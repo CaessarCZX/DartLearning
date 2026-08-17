4f9876738cfbac28053936637842732b0c21295f
CesarTun <ctuncetina@gmail.com>
2026-08-16 19:41:14 -0600

    bug: deleted files restored

---
+++ counter_app/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+/// Main function is the root of the app
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // Constructor of current widget, includes a param called 'key' which is used to map
+  // the widget en the app's widget Tree
+  // Always to use 'const' as type of data for the constructor in a StatelessWidget
+  // This indicate that the values of widget never change and will be compiled in that way.
+  const MyApp({super.key});
+
+  @override
+  //All StatelessWidget and StatefulWidget must have a build method, this is the same thing as 'render'
+  Widget build(BuildContext context) {
+    // Material App is a reusable widget class with atrributes or methods 
+    // of 'Material Design UI'     
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // Scaffold is the common layout in most applications includes: appbar, wrap menu, etc.
+      home: Scaffold(
+        body: Center(child: Text('Hello world')),
+      ),
+    );
+  }
+}
